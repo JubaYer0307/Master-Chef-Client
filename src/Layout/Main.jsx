1bcbbf3101def02392daf32cf46c332676c0d6cd
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-05-03 00:59:39 +0600

    six card added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,30 +1,52 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Header from "../Pages/Shared/Header/Header";
 import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Main = () => {
+  const [chefs, setChefs] = useState([]);
 
-    const [chef, setChef] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/main")
+      .then((res) => res.json())
+      .then((data) => setChefs(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    useEffect( ()=>{
-        fetch('http://localhost:5000/main')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+  const chefCards = chefs.map((chef) => (
+    <Col key={chef.id} lg={4} md={6} className="mb-4">
+      <Card>
+        <Card.Img variant="top" src={chef.chefPicture} />
+        <Card.Body>
+          <Card.Title>{chef.chefName}</Card.Title>
+          <Card.Text>
+            Years of experience: {chef.yearsOfExperience}
+            <br />
+            Number of recipes: {chef.numberOfRecipes}
+            <br />
+            Likes: {chef.likes}
+          </Card.Text>
+          <Link to={chef.viewRecipesButton}>
+            <Button variant="primary">View recipes</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Col>
+  ));
 
-    },[])
+  const rows = [];
+
+  for (let i = 0; i < chefCards.length; i += 3) {
+    rows.push(<Row key={i}>{chefCards.slice(i, i + 3)}</Row>);
+  }
+
   return (
     <div>
       <Header></Header>
-      <Container>
-        
-        <Row>
-          <Col lg={12}>Main content</Col>
-         
-        </Row>
-      </Container>
-
+      <Container>{rows}</Container>
       <Footer></Footer>
     </div>
   );
