0a2bb47205d23b1090f5ffb992650ee7a7e584eb
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-05-02 19:17:57 +0600

    data fetching from server

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Header from "../Pages/Shared/Header/Header";
 import { Col, Container, Row } from "react-bootstrap";
 
 const Main = () => {
+
+    const [chef, setChef] = useState([]);
+
+    useEffect( ()=>{
+        fetch('http://localhost:5000/main')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+
+    },[])
   return (
     <div>
       <Header></Header>
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import superfood from "../../../assets/superfood.png";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button } from "react-bootstrap";
 
 const Header = () => {
   return (
@@ -7,6 +12,25 @@ const Header = () => {
       <div className="text-center">
         <img src={superfood} alt="" />
       </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Superfood</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">Blog</Nav.Link>
+              
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
